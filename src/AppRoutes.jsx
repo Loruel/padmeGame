ccaf6b920d895a64c6c177b68ee0067298e9236f
Loruel <paco.jeansl@gmail.com>
2024-10-18 12:33:35 -0600

    correcion home 2

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
-import Home from './pages/home'
 import Multiplicacion from './pages/Multiplicacion'
+import HomePage from './pages/HomePage'
+
+
 
 export default function AppRoutes() {
     return (
         <Routes>
-            <Route path='/' element={<Home />} />
+            <Route path='/' element={<HomePage />} />
             <Route path='/multiply' element={<Multiplicacion />} />
 
             <Route path='*' element={<Navigation to='/' />} />
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-export default function Home() {
+export default function HomePage() {
     return (
         <div className='bg-blue-700 w-full h-screen flex justify-center items-center'>
 
